1ea9f58d7ed231fb7647cdf53b811018681a672e
mohamedeliwa <mohamed.av11@gmail.com>
2023-02-23 11:42:29 +0200

    Extracting Logic from main

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
-use std::{env, fs};
+use std::{env, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::build(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments {err}");
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
-
-    println!("{}", contents);
+    run(config);
 }
 
 struct Config {
@@ -29,4 +29,11 @@ impl Config {
 
         Ok(Config { query, file_path })
     }
+}
+
+fn run(config: Config) {
+    let contents =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
 }
